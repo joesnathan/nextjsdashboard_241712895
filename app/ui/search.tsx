24b1dd2937717a2ca8c58798295099770f6a310f
joesnathan <Jonathanindrawan224@gmail.com>
2026-05-21 17:07:33 +0700

    benerin search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,20 +9,21 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
+  // useDebouncedCallback dideklarasikan langsung sebagai fungsi handler utama
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
+    
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
-}
+  }, 300); // Menunda eksekusi URL selama 300ms agar performa ketikan lancar
+
   return (
     <div className="relative flex flex-1 shrink-0">
       <label htmlFor="search" className="sr-only">
